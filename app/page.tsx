0f857314c667d35dc7c58ad593e79769510f9767
Matt <solomatias321@gmail.com>
2025-12-30 11:32:14 +0100

    feat: add feautures, pricing cards in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
 import { SignedIn, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 import Hero from "@/components/landing/hero";
+import { Features } from "@/components/landing/features";
+import { HowItWorks } from "@/components/landing/how-it-works";
+import { CTA } from "@/components/landing/cta";
+import { Pricing } from "@/components/landing/pricing";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white dark:bg-black">
       <Hero />
+      <Features />
+      <HowItWorks />
 
       <SignedIn>
         <div className="fixed top-4 right-4">
           <UserButton />
         </div>
       </SignedIn>
+
+      <Pricing />
+      <CTA />
     </main>
   );
 }
